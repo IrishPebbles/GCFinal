d2d68b12a87ca8dd9ef4d4886ba033bf20560ce8
Jenna Price <8579779+jennaprice@users.noreply.github.com>
2017-12-14 12:21:04 -0500

    Front end looking

--- WebContent/resources/passwordvalidation.js
+++ WebContent/resources/passwordvalidation.js
@@ -1,24 +1,21 @@
-<script>
+
 var myInput = document.getElementById ("psw");
 		var letter = document.getElementById ("letter");
 		var capital = document.getElementById ("capital");
 		var number = document.getElementById ("number");
-		var length = document.getElementById ("length"); // When the user clicks on the password field,
-	show the message box
+		var length = document.getElementById ("length"); // When the user clicks on the password field, show the message box
 		myInput.onfocus = function () {document
 	.getElementById("message").style.display = "block";
 	
 }
 
-//
-When the user clicks outside of the password field, hide the message box
+//When the user clicks outside of the password field, hide the message box
 		myInput.onblur = function () {document
 	.getElementById("message").style.display = "none";
 	
 }
 
-//
-When the user starts to type something inside the password field
+//When the user starts to type something inside the password field
 		myInput.onkeyup = function () { // Validate lowercase letters var
 	lowerCaseLetters = /[a-z]/g;if (myInput.value.match(lowerCaseLetters))
 	{ letter.classList.remove("invalid");letter .classList.add("valid");
@@ -29,10 +26,10 @@ else {letter .classList.remove("valid");letter .classList.add("invalid");
 	
 }
 
-//
-Validate capital letters
-			var upperCaseLetters = /[A-Z] /g;
-			if (myInput.value.match (upperCaseLetters)) {capital
+//Validate capital letters
+var upperCaseLetters = /[A-Z] /g;
+if (myInput.value.match (upperCaseLetters)) {
+	capital
 	.classList.remove("invalid");capital .classList.add("valid");
 	
 }
@@ -42,51 +39,28 @@ else {capital .classList.remove("valid");capital
 	
 }
 
-//
-Validate numbers
+//Validate numbers
 			var numbers = /[ 0-9] /g;
-			if (myInput.value.match (numbers)) {number
-	.classList.remove("invalid");number .classList.add("valid");
+			if (myInput.value.match (numbers)) {
+				number.classList.remove("invalid");
+				number .classList.add("valid");
+	
+			}
+
+			else {number .classList.remove("valid");number .classList.add("invalid");
 	
 }
 
-else {number .classList.remove("valid");number .classList.add("invalid");
-	
-}
-
-//
-Validate
- 
-length
+//Validate length
 
 			
-if
- 
-(
-myInput
-.value
-.length
->
-=
-8)
-{
-length
-.classList
-.remove
-("invalid");
+if(myInput.value.length>=8) {
+	length.classList.remove("invalid");
 
-				
-length
-.classList
-.add
-("valid");
+	length.classList.add("valid");
+}
 
-			
+else {
+	length .classList.remove("valid");length .classList.add("invalid");
+	}
 }
-else {length .classList.remove("valid");length .classList.add("invalid");
-	
-}
-}
-</
-script
->
